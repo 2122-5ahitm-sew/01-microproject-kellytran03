375d95dbc12b46013f419c1e02af351a4a219a6c
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2021-09-26 22:40:45 +0200

    implemented contructor, getter & setter and toString in Call.java

--- src/main/java/callcenter/entity/Call.java
+++ src/main/java/callcenter/entity/Call.java
@@ -1,4 +1,47 @@
 package callcenter.entity;
 
 public class Call {
+    private Long id;
+
+    private Customer customer;
+
+    private Employee employee;
+
+    public Call(Customer customer, Employee employee) {
+        this.customer = customer;
+        this.employee = employee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public String toString() {
+        return "Call{" +
+                "id=" + id +
+                ", customer=" + customer +
+                ", employee=" + employee +
+                '}';
+    }
 }
