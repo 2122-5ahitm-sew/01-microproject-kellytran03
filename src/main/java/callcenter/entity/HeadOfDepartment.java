2829469c42a3d5908eaf734f99358bd405b1c282
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2021-09-26 22:48:14 +0200

    implemented contructor, getter & setter and toString in HeadOfDepartment.java

--- src/main/java/callcenter/entity/HeadOfDepartment.java
+++ src/main/java/callcenter/entity/HeadOfDepartment.java
@@ -1,4 +1,60 @@
 package callcenter.entity;
 
 public class HeadOfDepartment {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Department department;
+
+    public HeadOfDepartment(String firstName, String lastName, Department department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "HeadOfDepartment{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", department=" + department +
+                '}';
+    }
 }
